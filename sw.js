9f33ed46ed47ccf09cb5e4e34f0548015bab0a18
mis-finanzas <johansan99@gmail.com>
2026-04-13 15:58:23 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'finanzas-v4.5';
+const CACHE_NAME = 'misfinanzas-v5';
 const ASSETS = ['./index.html', './offline.html', './manifest.json'];
 
 self.addEventListener('install', event => {
