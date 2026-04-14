3640248ff81f7238cfe8e560123fe8c12a1c05b0
mis-finanzas <johansan99@gmail.com>
2026-04-13 23:27:27 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,44 +1,10 @@
-const CACHE_NAME = 'misfinanzas-v5';
-const ASSETS = ['./index.html', './offline.html', './manifest.json'];
+const CACHE_NAME = 'misfinanzas-v2';
+const ASSETS = ['./index.html', './manifest.json'];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => 
-      Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
-      )
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request)
-        .catch(() => caches.match('./index.html').then(res => res || caches.match('./offline.html')))
-    );
-    return;
-  }
-  
-  event.respondWith(
-    caches.match(event.request)
-      .then(cached => {
-        if (cached) return cached;
-        return fetch(event.request)
-          .then(response => {
-            const clone = response.clone();
-            caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-            return response;
-          })
-          .catch(() => caches.match('./offline.html'));
-      })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
